e9602646430a45f7ea80af39c671dbebb3d7616b
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2021-02-23 17:59:07 +0200

    Add sphere rendering.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,19 @@ use ray::Ray;
 use std::io::{self, Write};
 use vec3::{Color, Point3, Vec3};
 
+fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> bool {
+    let oc = ray.origin - center;
+    let a = Vec3::dot(&ray.direction, &ray.direction);
+    let b = 2.0 * Vec3::dot(&oc, &ray.direction);
+    let c = Vec3::dot(&oc, &oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
+
 fn ray_color(ray: Ray) -> Color {
+    if hit_sphere(&Point3::with_values(0.0, 0.0, -1.0), 0.5, &ray) {
+        return Color::with_values(1.0, 0.0, 0.0);
+    }
     let unit_direction: Vec3 = Vec3::unit_vector(&ray.direction);
     let t = 0.5 * (unit_direction.y + 1.0);
     (1.0 - t) * Color::with_values(1.0, 1.0, 1.0) + t * Color::with_values(0.5, 0.7, 1.0)
