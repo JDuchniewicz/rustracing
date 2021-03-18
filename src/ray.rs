53ab2bc361773c21415581d693fb023beb36da64
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2021-03-18 15:22:13 +0100

    Revert "Revert "Some performance improvements and more idiomatic code in some places""

--- src/ray.rs
+++ src/ray.rs
@@ -1,26 +1,22 @@
 use crate::vec3::{Point3, Vec3};
 
+#[derive(Default)]
 pub struct Ray {
     pub origin: Point3,
     pub direction: Vec3,
 }
 
 impl Ray {
-    pub fn new() -> Ray {
-        Ray {
-            origin: Point3::new(),
-            direction: Vec3::new(),
-        }
-    }
-
-    pub fn with_values(orig: Point3, dir: Vec3) -> Ray {
+    #[inline]
+    pub fn new(orig: Point3, dir: Vec3) -> Ray {
         Ray {
             origin: orig,
             direction: dir,
         }
     }
 
+    #[inline]
     pub fn at(&self, t: f64) -> Point3 {
-        Point3::with_vec3(self.origin + t * self.direction)
+        self.origin + t * self.direction
     }
 }
